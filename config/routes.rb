bb56fb89a458ae11582b244bce20d64ace41c0e9
tshuttles <tshuttlesworth@gmail.com>
2019-12-09 18:42:25 -0600

    add champion generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :champions
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20191210004140_create_champions.rb
@@ -0,0 +1,12 @@
+class CreateChampions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :champions do |t|
+      t.string :name
+      t.string :class
+      t.string :race
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end

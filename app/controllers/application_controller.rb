101d22ead391b1cee57b95fd86fb45b8496674b4
tshuttles <tshuttlesworth@gmail.com>
2019-12-10 00:07:52 -0600

    set up home view/root and app helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  
+  def home 
+  end 
+
+  private 
+
+  def current_user 
+  end 
+
+  def logged_in? 
+  end 
+
+  def authenticate_user 
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :inventory_items
   resources :champions
   resources :users
+
+  root 'application#home' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

ea07c13187172b540864032dfcc03de70bac753c
tshuttles <tshuttlesworth@gmail.com>
2019-12-11 16:23:24 -0600

    create helper methods in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,16 @@ class ApplicationController < ActionController::Base
   private 
 
   def current_user 
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end 
 
-  def logged_in? 
+  def logged_in?
+    !!current_user
   end 
 
   def authenticate_user 
+    if !logged_in?
+      redirect_to root_path
   end 
 
 end

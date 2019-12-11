44b1cec0044514d4f29e049b2dccf79a345d71fe
tshuttles <tshuttlesworth@gmail.com>
2019-12-10 22:12:01 -0600

    fill out sessions controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController 
 
   def login
+    @user = User.new 
   end 
 
   def create 
+    @user = User.find_by(name: params[:user][:name])
+    if @user && user.authenticate[:user][:password] 
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else 
+      render 'login' 
+    end 
   end 
 
   def destroy 
+    session.delete :user_id 
+    redirect_to root_path
   end 
 
 end 

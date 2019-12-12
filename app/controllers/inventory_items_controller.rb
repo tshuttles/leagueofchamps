0066651e1fce338caaab2005539209e29193c7c8
tshuttles <tshuttlesworth@gmail.com>
2019-12-11 23:02:18 -0600

    create champion view files and set up champion index and add prelim footer of app

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -1,2 +1,3 @@
 class InventoryItemsController < ApplicationController
+  
 end
--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,2 +1,17 @@
 class WeaponsController < ApplicationController
+
+  def new 
+    @weapon = Weapon.new 
+  end 
+
+  def create 
+    @weapon = Weapon.new 
+    @weapon.save ? (redirect_to weapon_path(@weapon)) : (redirect_to new_weapon_path)
+  end 
+
+  def delete
+    Weapon.find(params[:id]).destroy 
+    redirect_to weapon_path(@weapon)
+  end 
+
 end
